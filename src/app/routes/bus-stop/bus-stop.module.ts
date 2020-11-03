71f5c7446387f68da7bae6e9fffa1e5e8f2ee873
ZealousCynic <39732453+ZealousCynic@users.noreply.github.com>
2020-11-03 08:44:56 +0100

    Revert "Feature 01 stoppested crud"

--- src/app/routes/bus-stop/bus-stop.module.ts
+++ src/app/routes/bus-stop/bus-stop.module.ts
@@ -3,9 +3,8 @@ import { SharedModule } from '@shared/shared.module';
 import { BusStopRoutingModule } from './bus-stop-routing.module';
 import { BusStopStopOverviewComponent } from './stop-overview/stop-overview.component';
 import { BusStopStopInfoModifyComponent } from './stop-info-modify/stop-info-modify.component';
-import { BusStopBusStopDeleteDialogComponent } from './bus-stop-delete-dialog/bus-stop-delete-dialog.component';
 
-const COMPONENTS = [BusStopStopOverviewComponent, BusStopStopInfoModifyComponent, BusStopBusStopDeleteDialogComponent];
+const COMPONENTS = [BusStopStopOverviewComponent, BusStopStopInfoModifyComponent];
 const COMPONENTS_DYNAMIC = [];
 
 @NgModule({
